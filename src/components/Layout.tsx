0dc9535fd3adaaff9ad157c3ef77f3287d2d02e1
chae-chae <abed_nadir@naver.com>
2023-11-21 11:45:01 +0900

    add Layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,31 @@
+import { Link } from "gatsby";
+import React, { ReactElement, ReactNode } from "react";
+
+interface ILayoutProps {
+  children: ReactNode;
+  title: string;
+}
+
+export default function Layout({ children, title }: ILayoutProps) {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about-me">About Me</Link>
+          </li>
+          <li>
+            <Link to="https://github.com/chae-chae">Github</Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1>{title}</h1>
+        {children}
+      </main>
+    </div>
+  );
+}
---
+++ src/pages/about-me.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Layout from "../components/Layout";
+
+export default function AboutMe() {
+  return (
+    <Layout title="this is about me">
+      <div></div>
+    </Layout>
+  );
+}
