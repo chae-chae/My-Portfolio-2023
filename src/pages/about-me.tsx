5e3cc2d9c6aab5bbc7eca2a29dbfad0e4b11f97d
chae-chae <abed_nadir@naver.com>
2023-11-22 11:33:47 +0900

    about page

--- src/pages/about-me.tsx
+++ src/pages/about-me.tsx
@@ -1,10 +1,21 @@
 import * as React from "react";
 import Layout from "../components/Layout";
+import Seo from "../components/Seo";
+import { Link } from "gatsby";
 
 export default function AboutMe() {
   return (
-    <Layout title="this is about me">
-      <div></div>
+    <Layout title="About Me">
+      <article>
+        <h2>Jeong ChaeKyeong</h2>
+        <h4>Junior FrontEnd Engineer</h4>
+        <h5>abed_nadir@naver.com</h5>
+        <Link to="https://github.com/chae-chae">
+          <h5>Go to Github</h5>
+        </Link>
+      </article>
     </Layout>
   );
 }
+
+export const Head = () => <Seo title="About Me" />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import Layout from "../components/Layout";
+import Seo from "../components/Seo";
 
 export default function IndexPage() {
   return (
     <Layout title="HOME">
-      <h1>Hello! This is chae-chae's Portfolio Site.</h1>
+      <h1>Hello! This is Jeong ChaeKyeong's Portfolio Site.</h1>
     </Layout>
   );
 }
+
+export const Head = () => <Seo title="Home" />;
