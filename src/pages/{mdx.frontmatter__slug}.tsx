192e882e5125f820c423acdb8f6e26477c232c58
chae-chae <abed_nadir@naver.com>
2023-11-24 12:21:57 +0900

    add imgs and edit query

--- src/pages/{mdx.frontmatter__slug}.tsx
+++ src/pages/{mdx.frontmatter__slug}.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
 import Layout from "../components/Layout";
 import { graphql } from "gatsby";
+import Seo from "../components/Seo";
 
 interface IGithubPostProps {
   data: Queries.PostDetailQuery;
@@ -16,18 +17,25 @@ export default function GithubPost({ data, children }: IGithubPostProps) {
 }
 
 export const query = graphql`
-  query PostDetail {
-    allMdx {
-      nodes {
-        frontmatter {
-          title
-          tech_stack
-          lecture
-          date
-          author
-          slug
+  query PostDetail($frontmatter__slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
+      frontmatter {
+        title
+        tech_stack
+        lecture
+        date
+        author
+        slug
+        headerImage {
+          childImageSharp {
+            gatsbyImageData
+          }
         }
       }
     }
   }
 `;
+
+export const Head = ({ data }: IGithubPostProps) => (
+  <Seo title={data.mdx?.frontmatter?.title!} />
+);
